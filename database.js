ba1556d0b8242055a6e39f9f571f2667ece9a0b2
Zeng Tao <qingchun1995@gmail.com>
2020-02-15 20:00:03 +0800

    Create User model

---
+++ database.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const server = 'Leo:db19950723@cluster0-f93qf.mongodb.net' // REPLACE WITH YOUR DB SERVER
+const database = 'fcc-Mail'      // REPLACE WITH YOUR DB NAME
+
+class Database {
+  constructor() {
+    this._connect()
+  }
+
+  _connect() {
+    mongoose
+      .connect(
+        `mongodb+srv://${server}`,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+        }
+      )
+      .then(() => {
+        console.log('Database connection successful')
+      })
+      .catch(err => {
+        console.error(`Database connection error: ${err}`)
+      })
+  }
+}
+
+module.exports = new Database()
