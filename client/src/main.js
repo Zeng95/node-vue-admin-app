1e07f3d61f57164376dff6afd27c8df87e0fc696
Zeng Tao <qingchun1995@gmail.com>
2020-02-21 15:52:02 +0800

    Use the Bootstrap-Vue Vue CLI 3 plugin

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./assets/css/reset.css";
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import "./plugins/bootstrap-vue";
 
 Vue.config.productionTip = false;
 
