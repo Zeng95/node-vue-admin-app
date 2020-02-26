cb58e389a95214e8e6f25ae0ffe53bf13bc3b14a
Zeng Tao <qingchun1995@gmail.com>
2020-02-26 09:30:10 +0800

    Install Vuelidate

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
+import './assets/css/custom.css'
+import './assets/css/utility.css'
 
+Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
 new Vue({
