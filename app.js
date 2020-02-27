6ba9bcf7bffd360c49806c1508a7cf24155bea34
Zeng Tao <qingchun1995@gmail.com>
2020-02-27 14:22:47 +0800

    Install CORS and enable it

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // 搭建本地服务器
 const express = require('express')
+const cors = require('cors')
 
 require('./database')
 
 const app = express()
 const port = 5000
 
-const usersRouter = require('./routes/api/users')
+const authRouter = require('./routes/api/auth')
 
+const corsOptions = {
+  origin: "http://localhost:8080"
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
